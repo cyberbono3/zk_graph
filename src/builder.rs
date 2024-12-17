1ab883b0274f18e90f4a378ee9712b4b7882dfa9
Andrei Ivasko <andrei@fantom.foundation>
2024-12-17 15:01:07 +0300

    example2

--- src/builder.rs
+++ src/builder.rs
@@ -139,6 +139,25 @@ mod tests {
         let result = builder.add(x_squared_plus_5, x);
 
         builder.fill_nodes(&[(x, 3)]);
-        assert_eq!(builder.get_value(&result), Some(17)); // Should print 17 (3^2 + 3 + 5)
+        assert_eq!(builder.get_value(&result), Some(17)); 
+    }
+
+    #[test]
+    fn example_2() {
+         // Example 2: f(a) = (a+1) / 8
+        let mut builder = Builder::new();
+        let a = builder.init();
+        let one = builder.constant(1);
+        let b = builder.add(a, one);
+
+        let c = builder.hint(move |values| values.get(&b.id()).map(|b_val| b_val / 8));
+
+        let eight = builder.constant(8);
+        let c_times_8 = builder.mul(c, eight);
+        builder.assert_equal(b, c_times_8);
+
+        builder.fill_nodes(&[(a, 7)]);
+        assert!(builder.check_constraints());
+        assert_eq!(builder.get_value(&c), Some(1)); 
     }
 }
